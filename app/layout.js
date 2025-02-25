6105e3e3ff6ec476a767c07401307f5d90dec511
Detims <2005.nnhan@gmail.com>
2025-02-25 10:53:04 -0800

    color change

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body
-        className={`${ovo.className} ${openSans.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
+        className={`${ovo.className} ${openSans.className} antialiased leading-8 overflow-x-hidden dark:bg-stone-800 dark:text-white`}
       >
         {children}
       </body>
